f02be0cedbaa9c215dca9118b2167d73366c0370
alexherraiz <alexander_herraiz@hotmail.com>
2017-09-22 19:24:16 -0400

    Committing Everything!

---
+++ aherraiz/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { WorkComponent } from './work/work.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'work', component: WorkComponent },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    WorkComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
